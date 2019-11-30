a8fc8f1de3c1ebc90d02385af66a742b82aea630
Isaiah Callano: Laptop <sandrockxd@gmail.com>
2019-11-30 11:13:41 -0800

    add addition and equals functionality

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const accInput = document.querySelector('.acc-input');
 const curInput = document.querySelector('.cur-input');
+let computer = [];
+let current;
+let sum;
+let lastOp;
+
 
 // Numbers
 
 function assignNum(numVar, num) {
     numVar.addEventListener('click', () => {
         curInput.innerHTML += num;
+        current = parseInt(curInput.innerHTML);
+        console.log('computer: ' + computer, '| current: ' + current);
     })
 }
 
@@ -24,6 +31,8 @@ assignNum(n9, 9);
 c.addEventListener('click', () => {
     accInput.innerHTML = '';
     curInput.innerHTML = '';
+    current = undefined;
+    computer = [];
 });
 
 ce.addEventListener('click', () => {
@@ -33,22 +42,46 @@ ce.addEventListener('click', () => {
 
 // Operators
 
+
+
 add.addEventListener('click', () => {
-    accInput.innerHTML += curInput.innerHTML;
-    accInput.innerHTML += '+';
+    accInput.innerHTML += curInput.innerHTML + '+';
+    curInput.innerHTML = '';
+    computer.push(current);
+    if (computer.length > 1) {
+        sum = computer[0] + computer[1];
+        computer = [sum];
+    }
+    lastOp = "add";
+    current = undefined;
+    console.log('computer: ' + computer, '| current: ' + current);
 })
 
 subtract.addEventListener('click', () => {
     accInput.innerHTML += curInput.innerHTML;
     accInput.innerHTML += '-';
+    curInput.innerHTML = '';
 })
 
 multiply.addEventListener('click', () => {
     accInput.innerHTML += curInput.innerHTML;
     accInput.innerHTML += '*';
+    curInput.innerHTML = '';
 })
 
 divide.addEventListener('click', () => {
     accInput.innerHTML += curInput.innerHTML;
     accInput.innerHTML += '&#247';
+    curInput.innerHTML = '';
+})
+
+equals.addEventListener('click', () => {
+    accInput.innerHTML = '';
+    if (current) {
+        if (lastOp = "add") {
+            computer = parseInt(computer) + current;
+        }
+    }
+    console.log('computer: ' + computer + '| current: ' + current);
+    curInput.innerHTML = computer;
 })
